d4452e8a41fd4781acb54ba37b128bf6989c7653
tumb1er <zimbler@gmail.com>
2016-11-25 10:25:59 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='aioworkerpool',
+    version='0.1',
+    packages=['aioworkerpool'],
+    url='https://github.com/tumb1er/aioworkerpool',
+    license='Beer license',
+    author='Tumbler',
+    author_email='zimbler@gmail.com',
+    description='Asynchronous master/worker for asyncio'
+)
